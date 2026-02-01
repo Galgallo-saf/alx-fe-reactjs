149fbd2819e46b2535f313db51092b1e450c1691
Galgallo-saf <galgallosafaro5@gmail.com>
2026-02-01 13:48:41 +0300

    Keep only one ALX React App folder, remove nested folders

--- alx-react-app-props/src/components/Footer.jsx
+++
@@ -1,9 +0,0 @@
-function Footer() {
-  return (
-    <footer style={{ backgroundColor: '#222', color: 'white', textAlign: 'center' }}>
-      <p>Footer Content</p>
-    </footer>
-  );
-}
-
-export default Footer;
