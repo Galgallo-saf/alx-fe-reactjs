63baacb238d45c1d4acb056a498edeb679ad1886
Galgallo-saf <galgallosafaro5@gmail.com>
2026-02-01 13:48:41 +0300

    Keep only one ALX React App folder, remove nested folders

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-
-import ProfilePage from "./components/ProfilePage";
-import UserContext from "./UserContext";
+import UserContext from "./context/UserContext";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
 
 function App() {
   const userData = {
