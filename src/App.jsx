aa12b95f2f634b5432552432a382619464bac355
Galgallo-saf <galgallosafaro5@gmail.com>
2026-02-03 20:09:39 +0300

    Refactor prop drilling using Context API

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
-import UserContext from "./context/UserContext";
-import Header from "./components/Header";
-import MainContent from "./components/MainContent";
-import Footer from "./components/Footer";
+import ProfilePage from "./components/ProfilePage";
+import UserContext from "./UserContext";
 
 function App() {
   const userData = {
