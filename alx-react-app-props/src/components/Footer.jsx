8fd41a9366087b650bf8544371f1e2747da4dd8b
Galgallo-saf <galgallosafaro5@gmail.com>
2026-02-03 23:17:43 +0300

    Refactor prop drilling using Context API

---
+++ alx-react-app-props/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: '#222', color: 'white', textAlign: 'center' }}>
+      <p>Footer Content</p>
+    </footer>
+  );
+}
+
+export default Footer;
