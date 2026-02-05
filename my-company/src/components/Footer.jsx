ec7cc00ea47f4987d29617a6deb6172521d37e23
Galgallo-saf <galgallosafaro5@gmail.com>
2026-02-05 21:50:05 +0300

    Build simple company website with React Router

---
+++ my-company/src/components/Footer.jsx
@@ -0,0 +1,17 @@
+function Footer() {
+  return (
+    <footer
+      style={{
+        marginTop: '40px',
+        padding: '15px',
+        backgroundColor: '#222',
+        color: '#fff',
+        textAlign: 'center'
+      }}
+    >
+      <p>© 2026 My Company. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
