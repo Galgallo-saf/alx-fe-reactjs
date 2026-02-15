2bfbbe5a30d5dc7f7caa2c05e9857103c3874438
Galgallo-saf <galgallosafaro5@gmail.com>
2026-02-15 22:39:35 +0300

    Fix tailwind content path to include public/index.html

--- recipe-sharing-platform/tailwind.config.js
+++ recipe-sharing-platform/tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}"
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
   ],
+  darkMode: false,
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
