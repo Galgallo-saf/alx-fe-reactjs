8132bb91739505b312e8eae487ac1dfd4fe59ee0
Galgallo-saf <galgallosafaro5@gmail.com>
2026-02-15 22:50:46 +0300

    Build Home Page with responsive recipe grid

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -1,16 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import HomePage from "./components/HomePage";
 
 function App() {
-  return (
-    <div className="flex items-center justify-center h-screen bg-gray-50">
-      <h1 className="text-4xl font-bold text-blue-500">
-        Hello Tailwind!
-      </h1>
-    </div>
-  );
+  return <HomePage />;
 }
 
 export default App;
