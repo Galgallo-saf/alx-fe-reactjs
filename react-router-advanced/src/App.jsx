0f2dbde1d29396181da5357671ff57d5cc02d09f
Galgallo-saf <galgallosafaro5@gmail.com>
2026-02-22 23:08:11 +0300

    Update App.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import BlogPost from "./components/BlogPost";
 
 function Home() {
   return <h2>Home Page</h2>;
@@ -14,12 +15,14 @@ function App() {
     <BrowserRouter>
       <nav>
         <Link to="/">Home</Link> |{" "}
-        <Link to="/profile">Profile</Link>
+        <Link to="/profile">Profile</Link> |{" "}
+        <Link to="/blog/1">Blog 1</Link>
       </nav>
 
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/blog/:id" element={<BlogPost />} />
       </Routes>
     </BrowserRouter>
   );
