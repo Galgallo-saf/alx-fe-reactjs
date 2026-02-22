5481217f3f112f552db70390e120d9d6b6608061
Galgallo-saf <galgallosafaro5@gmail.com>
2026-02-22 18:00:38 +0300

    Update App.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import Home from "./components/Home";
 import BlogPost from "./components/BlogPost";
+import Profile from "./components/Profile";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
   return (
@@ -9,12 +11,23 @@ function App() {
       <div>
         <nav>
           <Link to="/">Home</Link> |{" "}
-          <Link to="/blog/1">Blog Post 1</Link>
+          <Link to="/blog/1">Blog Post 1</Link> |{" "}
+          <Link to="/profile">Profile</Link>
         </nav>
 
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/blog/:id" element={<BlogPost />} />
+
+          {/* Protected profile route */}
+          <Route
+            path="/profile"
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
       </div>
     </BrowserRouter>
