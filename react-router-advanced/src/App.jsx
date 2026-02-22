6a6c34e18e82e1caf18ee788aa364f74d883597d
Galgallo-saf <galgallosafaro5@gmail.com>
2026-02-22 23:06:13 +0300

    Update App.jsx to contain Browser

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,33 +1,27 @@
-import { Routes, Route, Link } from "react-router-dom";
-import Home from "./components/Home";
-import Profile from "./components/Profile";
-import BlogPost from "./components/BlogPost";
-import ProtectedRoute from "./components/ProtectedRoute";
+import React from "react";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+function Home() {
+  return <h2>Home Page</h2>;
+}
+
+function Profile() {
+  return <h2>Profile Page</h2>;
+}
 
 function App() {
   return (
-    <div>
+    <BrowserRouter>
       <nav>
         <Link to="/">Home</Link> |{" "}
-        <Link to="/profile">Profile</Link> |{" "}
-        <Link to="/post/1">Blog Post 1</Link>
+        <Link to="/profile">Profile</Link>
       </nav>
 
       <Routes>
         <Route path="/" element={<Home />} />
-
-        <Route
-          path="/profile/*"
-          element={
-            <ProtectedRoute>
-              <Profile />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route path="/post/:id" element={<BlogPost />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
-    </div>
+    </BrowserRouter>
   );
 }
 
